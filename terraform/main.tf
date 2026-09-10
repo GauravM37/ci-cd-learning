cfc8a1458b51caddde2c87a4e4de9ba01277ab91
Gaurav <92116301+GauravM37@users.noreply.github.com>
2026-09-10 12:34:49 +0530

    Add Terraform CI

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+resource "local_file" "example" {
+  filename = "example.txt"
+  content  = "Hello from Terraform CI/CD"
+}
